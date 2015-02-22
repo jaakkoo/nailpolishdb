04060445675e157f27707747692b05db85d7a5e3
Jaakko Aro <jaakko.aro@saunalahti.fi>
2015-02-22 13:21:06 +0200

    Added possibility to define multiple workers
    Because I of course assume that this application will be super popular,
    added the possibility to define multiple iojs workers using throng.

--- index.js
+++ index.js
@@ -1,18 +1,29 @@
 var express = require('express');
+var throng = require('throng');
 var app = express();
+var WORKERS = process.env.WEB_CONCURRENCY || 1
+
 app.set('views', './views');
 app.set('view engine', 'jade')
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', function(request, response) {
-  response.render('index', {active: 'Home'});
+app.get('/', function(req, res) {
+  res.render('index', {active: 'Home'});
 });
 
-app.get('/about', function(request, response) {
-  response.render('index', {active: 'About'});
+app.get('/about', function(req, res) {
+  res.render('index', {active: 'About'});
 });
 
-app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'));
+function start() {
+  app.listen(app.get('port'), function() {
+    console.log("Node app is running at localhost:" + app.get('port'));
+  });
+}
+
+throng(start, {
+  workers: WORKERS,
+  lifetime: Infinity
 });
+
