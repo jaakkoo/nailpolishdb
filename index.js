62e516887eb638c99cdc79f94379364efca8f9d7
Jaakko Aro <jaakko.aro@saunalahti.fi>
2015-02-22 15:00:16 +0200

    Added new file polish.js
    Polish.js will be resposible for handling requests related to finding
    nailpolishes.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var throng = require('throng');
+var polishes = require('./polish');
 var app = express();
 var WORKERS = process.env.WEB_CONCURRENCY || 1
 
@@ -8,22 +9,24 @@ app.set('view engine', 'jade')
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
+app.use('/polish', polishes)
+
 app.get('/', function(req, res) {
-  res.render('index', {active: 'Home'});
+    res.render('index', {active: 'Home'});
 });
 
 app.get('/about', function(req, res) {
-  res.render('index', {active: 'About'});
+    res.render('about', {active: 'About'});
 });
 
 function start() {
-  app.listen(app.get('port'), function() {
-    console.log("Node app is running at localhost:" + app.get('port'));
-  });
+    app.listen(app.get('port'), function() {
+        console.log("Node app is running at localhost:" + app.get('port'));
+    });
 }
 
 throng(start, {
-  workers: WORKERS,
-  lifetime: Infinity
+    workers: WORKERS,
+    lifetime: Infinity
 });
 
